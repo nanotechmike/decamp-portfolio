aba3779bc79392d598f25535a14c73f9d9ce7fcd
Michael Orr <michaelorr@Michaels-MacBook-Pro.local>
2018-02-01 13:00:52 -0800

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = HyoTechViewTool::Renderer.copyright 'Michael Orr', 'All rights reserved'
+  end
 end
